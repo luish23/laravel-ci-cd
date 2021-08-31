6e699206ef60b917cf0a598a2ee4468195937a8b
root <root@vm2.development.com>
2021-08-31 18:10:56 -0300

    Inicio CI-CD Laravel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('users/{name}/{id?}', function ($name, $id = NULL) {
+    return "Su nombre es: $name y su id es: $id";
+});
